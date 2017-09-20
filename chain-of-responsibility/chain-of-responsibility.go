97e912d8e2caf14acfe2eb967b7198b5a7161e90
Giri Kuncoro <girikuncoro@gmail.com>
2017-09-20 14:54:18 -0700

    Refactor chain of responsibility

--- chain-of-responsibility/chain-of-responsibility.go
+++ chain-of-responsibility/chain-of-responsibility.go
@@ -1,38 +1,73 @@
 package main
 
-type Logger string
+import "fmt"
 
-const (
-	Info  Logger = "info"
-	Debug        = "debug"
-	Error        = "error"
-)
-
-type Log interface {
-	write(message string)
+type Logger interface {
+	write(message string) string
+	LogMessage(logger Logger, message string)
 }
 
 type AbstractLogger struct {
-	level      Logger
-	nextLogger *AbstractLogger
+	Logger
+	level      int
+	nextLogger Logger
 }
 
-func (self *AbstractLogger) SetNextLogger(nextLogger *AbstractLogger) {
+func (self *AbstractLogger) SetNextLogger(nextLogger Logger) {
 	self.nextLogger = nextLogger
 }
 
-func (self *AbstractLogger) LogMessage(level Logger, message string) {
-	if self.level <= level {
-		self.write(message)
-	}
+func (self *AbstractLogger) LogMessage(logger Logger, message string) {
+	fmt.Println(logger.write(message))
 	if self.nextLogger != nil {
-		self.nextLogger.LogMessage(level, message)
+		self.nextLogger.LogMessage(self.nextLogger, message)
 	}
 }
 
-func (self *AbstractLogger) write(message string) {}
+type ConsoleLogger struct {
+	*AbstractLogger
+}
 
-// TODO: implement pattern usage
+func (self *ConsoleLogger) write(message string) string {
+	return "Standard Console::Logger: " + message
+}
+
+func NewConsoleLogger() *ConsoleLogger {
+	return &ConsoleLogger{&AbstractLogger{}}
+}
+
+type ErrorLogger struct {
+	*AbstractLogger
+}
+
+func (self *ErrorLogger) write(message string) string {
+	return "Error Console::Logger: " + message
+}
+
+func NewErrorLogger() *ErrorLogger {
+	return &ErrorLogger{&AbstractLogger{}}
+}
+
+type FileLogger struct {
+	*AbstractLogger
+}
+
+func (self *FileLogger) write(message string) string {
+	return "File::Logger: " + message
+}
+
+func NewFileLogger() *FileLogger {
+	return &FileLogger{&AbstractLogger{}}
+}
+
 func main() {
+	errorLogger := NewErrorLogger()
+	fileLogger := NewFileLogger()
+	consoleLogger := NewConsoleLogger()
+	errorLogger.SetNextLogger(fileLogger)
+	fileLogger.SetNextLogger(consoleLogger)
 
+	consoleLogger.LogMessage(consoleLogger, "This is an information")
+	fileLogger.LogMessage(fileLogger, "This is a debug level information")
+	errorLogger.LogMessage(errorLogger, "This is an error information")
 }
