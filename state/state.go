abf43c474c66d6977c31038b3a5f328751a54648
Giri Kuncoro <girikuncoro@gmail.com>
2017-10-06 22:27:00 -0700

    Add state pattern

--- state/state.go
+++ state/state.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import "fmt"
 
+type State interface {
+	Action(*Context)
+	Move()
+}
+
+type Context struct {
+	state State
+}
+
+type RunningState struct{}
+
+func (self *RunningState) Action(context *Context) {
+	fmt.Println("Robot is in running state")
+	context.state = self
+}
+
+func (self *RunningState) Move() {
+	fmt.Println("Robot runs 10 m/s")
+}
+
+type WalkingState struct{}
+
+func (self *WalkingState) Action(context *Context) {
+	fmt.Println("Robot is in walking state")
+	context.state = self
+}
+
+func (self *WalkingState) Move() {
+	fmt.Println("Robot walks 2 m/s")
+}
+
+func main() {
+	context := &Context{}
+
+	runningState := &RunningState{}
+	runningState.Action(context)
+
+	context.state.Move()
+
+	walkingState := &WalkingState{}
+	walkingState.Action(context)
+
+	context.state.Move()
 }
