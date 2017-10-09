9ec7a07a92948a46ed6bec80b6f7b4d591e32d17
Giri Kuncoro <girikuncoro@gmail.com>
2017-10-08 22:45:34 -0700

    Add mediator pattern

--- mediator/mediator.go
+++ mediator/mediator.go
@@ -1,16 +1,50 @@
 package main
 
+import "fmt"
+
 type Mediator interface {
-	SendMessage()
-	ShowMessage()
+	AddUser(User)
+	SendMessage(User, string)
 }
 
-type ChatMediator struct{}
+type Chatroom struct {
+	users []*User
+}
 
-type User struct{}
+func (self *Chatroom) AddUser(user *User) {
+	self.users = append(self.users, user)
+}
 
-type Chatroom struct{}
+func (self *Chatroom) SendMessage(sender *User, message string) {
+	for _, user := range self.users {
+		if user != sender {
+			fmt.Println(sender.name + " sent message to " + user.name + ": " + message)
+		}
+	}
+}
+
+type User struct {
+	name     string
+	mediator *Chatroom
+}
+
+func (self *User) Send(message string) {
+	self.mediator.SendMessage(self, message)
+}
+
+func NewUser(name string, mediator *Chatroom) *User {
+	user := &User{name, mediator}
+	mediator.AddUser(user)
+	return user
+}
 
 func main() {
+	chatroom := &Chatroom{}
+	larry := NewUser("Larry", chatroom)
+	mark := NewUser("Mark", chatroom)
+	bill := NewUser("Bill", chatroom)
 
+	larry.Send("Hello from Larry!")
+	mark.Send("Aloha from Mark!!")
+	bill.Send("Hi from Bill!!!")
 }
